a6e25463bc818c383275c1dae844872a008d1194
yugoslavskiy <yugoslavskiy@gmail.com>
2015-10-29 19:49:29 +0300

    modified:   osxdefaults.sh

--- mac_profiles/osxdefaults.sh
+++ mac_profiles/osxdefaults.sh
@@ -32,8 +32,8 @@ defaults write com.apple.LaunchServices LSQuarantine -bool false
 # Check for software updates daily, not just once per week
 defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
 
-# Disable Notification Center and remove the menu bar icon
-launchctl unload -w /System/Library/LaunchAgents/com.apple.notificationcenterui.plist 2> /dev/null
+# Enable Notification Center
+launchctl load -w /System/Library/LaunchAgents/com.apple.notificationcenterui.plist 2> /dev/null
 
 ###############################################################################
 # SSD-specific tweaks                                                         #
