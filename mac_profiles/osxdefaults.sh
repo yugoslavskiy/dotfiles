5fd657d344fa55e6d569cbc2d46ed81b4705865e
yugoslavskiy <yugoslavskiy@gmail.com>
2015-11-10 03:10:37 +0300

    modified:   mac_profiles/osxdefaults.sh

--- mac_profiles/osxdefaults.sh
+++ mac_profiles/osxdefaults.sh
@@ -15,7 +15,7 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Set computer name (as done via System Preferences → Sharing)
 sudo scutil --set ComputerName "wing"
-sudo scutil --set HostName "yugoslavskiy"
+sudo scutil --set HostName "wing"
 sudo scutil --set LocalHostName "yugoslavskiy"
 sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "wing"
 
