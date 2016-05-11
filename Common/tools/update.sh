a209e25815eb6919e46736f919038f17e89b4027
Yugoslavskiy Daniil <yugoslavskiy@gmail.com>
2016-05-11 18:02:48 +0300

    added .zshrc update

--- Common/tools/update.sh
+++ Common/tools/update.sh
@@ -4,6 +4,7 @@ printf "\n" "Updating Dotfiles"
 cd "~/.dotfiles"
 if git pull --rebase --stat origin master
 then
+	cp "$(uname -s)"/profile/.zshrc ~/.zshrc
   printf "\n" "Dotfiles has been updated and/or is at the current version."
 else
   printf "\n" 'There was an error updating.'
